859530311062c1a91c140fb7928e7f83ebbf3871
Yanuar Arifin <3326885+ynrfin@users.noreply.github.com>
2019-07-17 20:02:20 +0700

    create table barangs migration

---
+++ database/migrations/2019_07_16_120559_create_barangs_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBarangsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('barangs', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('kode');
+            $table->integer('stok');
+            $table->integer('harga');
+            $table->string('nama');
+            $table->text('deskripsi');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('barangs');
+    }
+}
