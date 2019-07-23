d8b578efa8753b70713bd555a3b8c078beacc811
Yanuar Arifin <3326885+ynrfin@users.noreply.github.com>
2019-07-24 06:13:06 +0700

    update resource format to jsonapi like

--- app/Http/Resources/Barang.php
+++ app/Http/Resources/Barang.php
@@ -11,13 +11,17 @@ class Barang extends JsonResource
     public function toArray($request)
     {
         return [
-            //'id' => $this->id,
-            //'kode' => $this->kode,
-            'nama' => $this->nama,
-            'deskripsi' => $this->deskripsi,
-            'harga' => $this->harga,
-            'stok' => $this->stok,
-            'created_at' => $this->created_at
+            'type' => 'barang',
+            'id' => (string)$this->id,
+            'atttributes' => [
+                'kode' => $this->kode,
+                'nama' => $this->nama,
+                'deskripsi' => $this->deskripsi,
+                'harga' => $this->harga,
+                'stok' => $this->stok,
+                'created_at' => $this->created_at,
+                'updated_at' => $this->updated_at
+            ]
         ];
     }
 }
