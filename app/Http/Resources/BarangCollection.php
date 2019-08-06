5db3e072900b729d8d8f1d180d2129fde32a7e4c
Yanuar Arifin <3326885+ynrfin@users.noreply.github.com>
2019-08-07 00:57:54 +0700

    add items object as data key only value. change later for compliance

--- app/Http/Resources/BarangCollection.php
+++ app/Http/Resources/BarangCollection.php
@@ -6,28 +6,12 @@ use Illuminate\Http\Resources\Json\ResourceCollection;
 
 class BarangCollection extends ResourceCollection
 {
-    //public static $wrap = "results";
     /* Transform resource to array
      */
     public function toArray($request)
     {
         return [
-            $this->collection
-            //'data' => $this->collection->transform(function($barang){
-            //    return [
-            //        'type' => 'barang',
-            //        'id' => (string)$barang->id,
-            //        'atttributes' => [
-            //            'kode' => $barang->kode,
-            //            'nama' => $barang->nama,
-            //            'deskripsi' => $barang->deskripsi,
-            //            'harga' => $barang->harga,
-            //            'stok' => $barang->stok,
-            //            'created_at' => $barang->created_at,
-            //            'updated_at' => $barang->updated_at
-            //        ]
-            //    ];
-            //})
+            'items' => $this->collection
         ];
     }
 }
