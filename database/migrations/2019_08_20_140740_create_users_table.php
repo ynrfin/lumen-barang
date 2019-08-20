6542b8f69f29258b6aed4723e386c410f309435d
Yanuar Arifin <3326885+ynrfin@users.noreply.github.com>
2019-08-20 21:48:20 +0700

    add users migration, factory, seeder, and add userseeder to databaseseeder

---
+++ database/migrations/2019_08_20_140740_create_users_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->text('name', 100);
+            $table->text('email', 100);
+            $table->text('password', 100);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
