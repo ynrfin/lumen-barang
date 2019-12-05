79c99cfe8b9921859aa96466ceb95cd3a9daefe6
Yanuar Arifin <yanuar@docotel.com>
2019-12-05 11:41:42 +0700

    could transform a list

---
+++ app/Transformers/CategoryTransformer.php
@@ -0,0 +1,29 @@
+<?php 
+
+namespace App\Transformers;
+
+use App\Category;
+use League\Fractal\TransformerAbstract;
+
+
+class CategoryTransformer extends TransformerAbstract
+{
+	public function transform(Category $category)
+	{
+		return [
+			'type' => 'category',
+            'id' => (string)$category->id,
+            'atttributes' => [
+                'name' => $category->name,
+                'created_at' => $category->created_at,
+                'updated_at' => $category->updated_at,
+                "related" => $category->barangs
+            ],
+            "links" => [
+                "self" => route('barang.one', ['id' => $category->id])
+            ]
+		];
+	}
+}
+
+ ?>
